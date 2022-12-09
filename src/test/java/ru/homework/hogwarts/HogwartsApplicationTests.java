1d21446328dba08393f3477a973a9351aae1d558
93294 <9329496@mail.ru>
2022-12-09 19:32:16 +0300

    Initial commit

---
+++ src/test/java/ru/homework/hogwarts/HogwartsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.homework.hogwarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
